4d86dedaac736c169a89d8f2c815e2573c575979
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-13 14:29:41 +0200

    adds Cast and Reviews components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,15 +4,20 @@ import SharedLayout from 'components/SharedLayout';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
+import Cast from 'components/Cast';
+import Reviews from 'components/Reviews';
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<SharedLayout/>}>
-          <Route index element={<Home/>} />
-          <Route path="movies" element={<Movies/>}/>
-          <Route path="movies/:movieId" element={<MovieDetails/>}/>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast/>}/>
+            <Route path="reviews" element={<Reviews/>}/>
+          </Route>
         </Route>
       </Routes>
     </div>
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,3 +1,18 @@
+import { Link, Outlet } from 'react-router-dom';
+
 export default function MovieDetails() {
-    return <div>MovieDetails</div>
+  return (
+    <section>
+      <h3>MovieDetails</h3>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet/>
+    </section>
+  );
 }
