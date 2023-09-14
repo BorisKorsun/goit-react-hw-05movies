95cd56f90cecd393973ba452ab5cb5dd571938fb
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-14 14:48:25 +0200

    adds propTypes to Suspnese Fallback

--- src/components/SuspenseFallback/SuspenseFallback.jsx
+++ src/components/SuspenseFallback/SuspenseFallback.jsx
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types'
+
 export default function SuspenseFallback({ children }) {
   return <p>{children}</p>;
+}
+
+SuspenseFallback.propTypes = {
+  children: PropTypes.string,
 }
